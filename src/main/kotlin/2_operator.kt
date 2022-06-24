68c885f226e3f71d9fca5a3d5ea1e677e4bc6abb
Azlannehh <agungazlan@gmail.com>
2022-06-24 23:31:03 +0700

    Pertemuan 1

---
+++ src/main/kotlin/2_operator.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //    komentar 1 baris
+    var a = 7
+    var b = 6
+    var c = (a++ + b++) / 2.0
+    print(c + b)
+    var dinamis = "hore"
+    var statis = 2.0
+    println(statis)
+}
